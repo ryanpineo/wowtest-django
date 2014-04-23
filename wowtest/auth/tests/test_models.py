0a2f13493638e3e52e818d4028a15ec8ff6196bc
Ryan Pineo <ryanpineo@gmail.com>
2014-04-23 18:49:35 -0400

    Add Account.check_password tests.

--- wowtest/auth/tests/test_models.py
+++ wowtest/auth/tests/test_models.py
@@ -5,9 +5,9 @@ from ..models import Account
 
 class AccountTests(TestCase):
     def test_can_create_with_username_email_password(self):
-        username = 'test'
-        password = 'test'
-        email = 'test@test.com'
+        username = 'testcreate'
+        password = 'testcreate'
+        email = 'testcreate@test.com'
 
         account = Account()
         account.username = username
@@ -15,3 +15,18 @@ class AccountTests(TestCase):
         account.email = email
         account.save()
         self.assertGreater(account.pk, 0)
+
+    def test_check_password_wrong_password(self):
+        account = Account()
+        account.set_password('test')
+        self.assertFalse(account.check_password('tester'))
+
+    def test_check_password_correct_password(self):
+        account = Account()
+        account.set_password('mongo')
+        self.assertTrue(account.check_password('mongo'))
+
+    def test_check_password_correct_password_different_case(self):
+        account = Account()
+        account.set_password('test')
+        self.assertTrue(account.check_password('tesT'))
